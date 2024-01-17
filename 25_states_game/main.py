f6d57c9ec9769182e6f806e22f08d145b432730a
Dima_Tatarenko <dmitriy.tatarenko@gmail.com>
2024-01-17 23:16:19 +0100

    day 25 challenge - improved the game to solve itself when you cannot remember more states

--- 25_states_game/main.py
+++ 25_states_game/main.py
@@ -6,7 +6,7 @@ states_csv = "E:/Programming/Python/daily_python/25_states_game/50_states.csv"
 
 states_data = pandas.read_csv(states_csv)
 state_list = states_data["state"]
-
+guessed_list = []
 
 
 # gray_squirrel_count = squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"]
@@ -22,13 +22,6 @@ pen = Turtle()
 pen.penup()
 pen.hideturtle()
 
-# def mclick_coords(x,y):
-#     print(x, y)
-
-# turtle.onscreenclick(mclick_coords)
-
-# turtle.mainloop()
-
 
 def question():
     answer_state = screen.textinput(title="Guess the state", prompt="Type a state name.")
@@ -36,6 +29,8 @@ def question():
 
 
 def check_answer(guess):
+    global guessed_list
+
     for state in state_list:
         if state == guess:
             state_info = states_data[states_data["state"] == guess]
@@ -45,15 +40,41 @@ def check_answer(guess):
 
             pen.goto(x_loc, y_loc)
             pen.write(f"{guess}", move=False, align="center", font=("Arial"))
+            guessed_list.append(guess)
+            print(guessed_list)
+            
+
+
+def show_answer():
+    for state in state_list:
+        if state not in guessed_list:
+            state_info = states_data[states_data["state"] == state]
+               
+            x_loc = state_info.x.values[0]
+            y_loc = state_info.y.values[0]
+
+            pen.color("red")
+            pen.goto(x_loc, y_loc)
+            pen.write(f"{state}", move=False, align="center", font=("Arial"))
 
 
 def gameLoop():
+    global guessed_list
     answer = question()
-
+    
     if answer == "stop":
         return
-    
+
+    if answer == "surrender":
+        show_answer()
+        return
+        
     check_answer(answer)
+
+    if len(guessed_list) == 49:
+        print("You win!")
+        return
+    
     gameLoop()
 
 
